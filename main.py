ad3fbbb90b1c895c130f504f8aaa99d963fe8991
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-03-25 22:26:21 +0100

    Include the endpoint as well as the database

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from database.postgresql_server_db import engine, Base
+from database.tables.exercise_table import Exercises
+from api.teacher_api import router as teacher_router
 
+# Start the server
 app = FastAPI()
 
+# End point to test the server
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+# Include the routers
+app.include_router(teacher_router)
+
+# Generate the tables of the db
+Base.metadata.create_all(bind=engine)
