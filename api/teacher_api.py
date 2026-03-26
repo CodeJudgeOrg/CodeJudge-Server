d9e523d021c0f48c5d9b1ba816e96d5499d28d12
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-03-26 20:39:02 +0100

    End point to receive all submissions

--- api/teacher_api.py
+++ api/teacher_api.py
@@ -3,16 +3,29 @@ from sqlalchemy.orm import Session
 from database.postgresql_server_db import SessionLocal
 from database import crud
 from schemas.exercise_schemas import ExerciseCreate, ExerciseCreateRespone
+from schemas.submission_schemas import SubmissionReceiveResponse
 
 router = APIRouter(prefix="/teacher", tags=["teacher"])
 
-def get_db():
+def getDB():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
+# ----------------------------------------------------
+# Exercises
+# ----------------------------------------------------
 @router.post("/uploadExercises", response_model=list[ExerciseCreateRespone])
-def uploadExercise(exercises: list[ExerciseCreate], db: Session = Depends(get_db)):
+def uploadExercise(exercises: list[ExerciseCreate], db: Session = Depends(getDB)):
     return crud.insertExercises(db, exercises)
+
+# ----------------------------------------------------
+# Submission
+# ----------------------------------------------------
+@router.get("/receiveSubmissions", response_model = list[SubmissionReceiveResponse])
+def receiveSubmissions(db: Session = Depends(getDB)):
+    return crud.receiveSubmissions(db)
+
+# TODO: Teacher should be able to edit an exercise, to delete exercises, to quit the session, etc.
