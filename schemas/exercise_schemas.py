c32730cdcee1db4b71ab4da8a86a96ab51c57a27
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-03-26 17:50:01 +0100

    Schema for the response of the new end point

--- schemas/exercise_schemas.py
+++ schemas/exercise_schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+# Schemas when creating an exercise
 class ExerciseCreate(BaseModel):
     name: str
     description: str
@@ -8,8 +9,21 @@ class ExerciseCreate(BaseModel):
     hint: str
     difficulty: int
 
-class ExerciseRespone(ExerciseCreate):
+class ExerciseCreateRespone(ExerciseCreate):
     id: int
+
+    class Config:
+        orm_mode = True
+
+# Schemas when receiving an exercise
+class ExerciseReceiveRespond(BaseModel):
+    id: int
+    name: str
+    description: str
+    task: str
+    solution: str
+    hint: str
+    difficulty: int
 
     class Config:
         orm_mode = True
