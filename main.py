de309064a3c53e6ef7fd5a7d13219545a22ff5ee
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-03-26 16:38:36 +0100

    Iclude the new endpoint and the new table

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from database.postgresql_server_db import engine, Base
 from database.tables.exercise_table import Exercises
+from database.tables.submission_table import SubmissionTable
 from api.teacher_api import router as teacher_router
+from api.student_api import router as student_router
 
 # Start the server
 app = FastAPI()
@@ -13,6 +15,7 @@ def health():
 
 # Include the routers
 app.include_router(teacher_router)
+app.include_router(student_router)
 
 # Generate the tables of the db
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind = engine)
