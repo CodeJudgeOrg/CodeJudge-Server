17feb7015ff224a2953f6d01e1cf2c70929bcfcb
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-03-26 20:13:53 +0100

    Upload a list of exercises

--- api/teacher_api.py
+++ api/teacher_api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.postgresql_server_db import SessionLocal
 from database import crud
-from schemas.exercise_schemas import ExerciseCreate, ExerciseRespone
+from schemas.exercise_schemas import ExerciseCreate, ExerciseCreateRespone
 
 router = APIRouter(prefix="/teacher", tags=["teacher"])
 
@@ -13,14 +13,6 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/uploadExercise", response_model=ExerciseRespone)
-def uploadExercise(exercise: ExerciseCreate, db: Session = Depends(get_db)):
-    return crud.createExercise(
-        db = db,
-        name = exercise.name,
-        description = exercise.description,
-        exercise = exercise.task,
-        solution = exercise.solution,
-        hint = exercise.hint,
-        difficulty = exercise.difficulty,
-    )
+@router.post("/uploadExercises", response_model=list[ExerciseCreateRespone])
+def uploadExercise(exercises: list[ExerciseCreate], db: Session = Depends(get_db)):
+    return crud.insertExercises(db, exercises)
